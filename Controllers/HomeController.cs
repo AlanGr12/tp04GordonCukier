a638c7a29a033a0d70a0caee12d63eae92934bec
ALAN GABRIEL GORDON <48860730@ort>
2025-05-08 15:53:43 -0300

    falta mucho, index casi terminado, model de juego casi terminado, controller terminado revisar el controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,13 +8,38 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+ 
+        public IActionResult Index()
+        {
+            return View(); 
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public IActionResult NuevaPartida()
+        {
+            Juego.IniciarNuevaPartida();
+            return View("Jugar");
+        }
+
+        public IActionResult Jugar()
+        {
+            if (Juego.JuegoTerminado)
+
+            return View(Juego.ObtenerEstado());
+        }
+
+        [HttpPost]
+        public IActionResult EnviarLetra(char letra)
+        {
+            Juego.IntentarLetra(letra);
+            return RedirectToAction("Jugar");
+        }
+
+        [HttpPost]
+        public IActionResult EnviarPalabra(string palabra)
+        {
+            Juego.IntentarPalabra(palabra);
+            return RedirectToAction("Jugar");
+        }
+
+      
 }
---
+++ Models/Juego.cs
@@ -0,0 +1,51 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TP04progGordonCukier.Models;
+
+namespace TP04progGordonCukier
+
+{
+public class Juego{
+
+public char letra{get; private set;}
+
+public string palabra{get; private set;}
+
+
+
+public juego(char letra, string palabra){
+
+
+this.letra = letra;
+
+this.palabra = palabra;
+}
+}
+}
+
+                                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
